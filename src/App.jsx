afd9b1f04e16bdabf3c757ec2d5056e09ee87ac0
Fellipe J. P. Oliveira <fellipe_ulkiorra@live.com>
2023-09-29 23:25:28 -0300

    Inicio do site

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <nav id="navbar">
+      <h2>
+        
+      </h2>
+    </nav>
+      <h2>Movie List</h2>
+    </div>
+  )
+}
+
+export default App
